23465709daca209183d4409728bfe59819e60f39
Max Brockbank <maxbrockbank1999@gmail.com>
2020-10-20 16:27:34 -0700

    add fading transition on the flashcards
    Co-authored-by: Joseph Giunta <jgnohack@gmail.com>

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,14 +1,30 @@
 $(document).ready(function(){
-  $("#FC1").click(function(){
-    $("#FC1 .term").toggle();
-    $("#FC1 .def").toggle();
+  $("#FC1 .term").click(function(){
+    $("#FC1 .term").fadeToggle();
+    $("#FC1 .def").delay(500).fadeToggle();
   })
-  $("#FC2").click(function(){
-    $("#FC2 .term").toggle();
-    $("#FC2 .def").toggle();
+  $("#FC1 .def").click(function(){
+    $("#FC1 .def").fadeToggle();
+    $("#FC1 .term").delay(500).fadeToggle();
   })
-  $("#FC3").click(function(){
-    $("#FC3 .term").toggle();
-    $("#FC3 .def").toggle();
+  $("#FC1 .der").click(function(){
+    $("#FC1 .def").fadeToggle();
+    $("#FC1 .term").delay(500).fadeToggle();
+  })
+  $("#FC2 .term").click(function(){
+    $("#FC2 .term").fadeToggle();
+    $("#FC2 .def").delay(500).fadeToggle();
+  })
+  $("#FC2 .def").click(function(){
+    $("#FC2 .def").fadeToggle();
+    $("#FC2 .term").delay(500).fadeToggle();
+  })
+  $("#FC3 .term").click(function(){
+    $("#FC3 .term").fadeToggle();
+    $("#FC3 .def").delay(500).fadeToggle();
+  })
+  $("#FC3 .def").click(function(){
+    $("#FC3 .def").fadeToggle();
+    $("#FC3 .term").delay(500).fadeToggle();
   })
 })
